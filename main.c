af2f8d351783914ce9c2f8bd54e6f0260959c035
Ib Havn <iha@via.dk>
2019-10-30 13:26:44 +0100

    Add comment

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #define TASK_MY_TASK_STACK				( configMINIMAL_STACK_SIZE )
 #define	TASK_MY_SECOND_TASK_STACK		( configMINIMAL_STACK_SIZE )
 
-
+/* Task Handles */
 TaskHandle_t _taskSecondHandle = NULL;
 
 // --------------------------------------------------------------------------------------
