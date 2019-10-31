f19b3b78403ac872ebffcfdf234de07390e74883
Ib Havn <iha@via.dk>
2019-10-31 15:16:36 +0100

    Add Solution to exercise 2

--- main.c
+++ main.c
@@ -4,75 +4,109 @@
 #include "semphr.h"
 
 /* Priorities at which the tasks are created. */
-#define TASK_MY_TASK_PRIORITY			( tskIDLE_PRIORITY + 1 )
-#define	TASK_MY_SECOND_TASK_PRIORITY	( tskIDLE_PRIORITY + 2 )
+#define TASK_A_PRIORITY			( tskIDLE_PRIORITY + 1 )
+#define	TASK_B_PRIORITY	( tskIDLE_PRIORITY + 2 )
+#define	TASK_C_PRIORITY		( tskIDLE_PRIORITY + 3 )
 /* Task stack sizes*/
-#define TASK_MY_TASK_STACK				( configMINIMAL_STACK_SIZE )
-#define	TASK_MY_SECOND_TASK_STACK		( configMINIMAL_STACK_SIZE )
+#define TASK_A_STACK				( configMINIMAL_STACK_SIZE )
+#define	TASK_B_STACK		( configMINIMAL_STACK_SIZE )
+#define	TASK_C_STACK		( configMINIMAL_STACK_SIZE )
 
 /* Task Handles */
-TaskHandle_t _taskSecondHandle = NULL;
+TaskHandle_t _taskBHandle = NULL;
 
 /* Semaphors */
-SemaphoreHandle_t _putsSem = NULL;
+SemaphoreHandle_t _aSem = NULL;
+SemaphoreHandle_t _bSem = NULL;
+SemaphoreHandle_t _cSem = NULL;
 
 // --------------------------------------------------------------------------------------
-void taskMyTask(void* pvParameters)
+void taskATask(void* pvParameters)
 {
 	// Remove compiler warnings.
 	(void)pvParameters;
 
 	for (;;)
 	{
-		if (xSemaphoreTake(_putsSem, portMAX_DELAY)) {
-			puts("Hi from My Task");
-			xSemaphoreGive(_putsSem);
-		}
-		vTaskDelay(pdMS_TO_TICKS(200));
+		puts("R");
+		xSemaphoreGive(_bSem);
+		xSemaphoreTake(_aSem, portMAX_DELAY);
+		puts("OK");
+
+		vTaskSuspend(NULL);
 	}
 }
 
 // --------------------------------------------------------------------------------------
-void taskMySeccondTask(void* pvParameters)
+void taskBTask(void* pvParameters)
 {
 	// Remove compiler warnings.
 	(void)pvParameters;
 
 	for (;;)
 	{
-		if (xSemaphoreTake(_putsSem, portMAX_DELAY)) {
-			puts("Hi from My Second Task");
-			xSemaphoreGive(_putsSem);
-		}
-		vTaskDelay(pdMS_TO_TICKS(1000));
+		xSemaphoreTake(_bSem, portMAX_DELAY);
+		puts("I");
+		xSemaphoreGive(_cSem);
+		xSemaphoreTake(_bSem, portMAX_DELAY);
+		puts("OK");
+
+		vTaskSuspend(NULL);
 	}
 }
 
+// --------------------------------------------------------------------------------------
+void taskCTask(void* pvParameters)
+{
+	// Remove compiler warnings.
+	(void)pvParameters;
+
+	for (;;)
+	{
+		xSemaphoreTake(_cSem, portMAX_DELAY);
+		puts("O");
+		puts("OK");
+		xSemaphoreGive(_aSem);
+		xSemaphoreGive(_bSem);
+		
+		vTaskSuspend(NULL);
+	}
+}
 // --------------------------------------------------------------------------------------
 void main(void)
 {
 	/* Create the task, not storing the handle. */
 	xTaskCreate(
-		taskMyTask,       /* Function that implements the task. */
-		"MyTask",          /* Text name for the task. */
-		TASK_MY_TASK_STACK,      /* Stack size in words, not bytes. */
+		taskATask,       /* Function that implements the task. */
+		"Task A",          /* Text name for the task. */
+		TASK_A_STACK,      /* Stack size in words, not bytes. */
 		(void*)1,    /* Parameter passed into the task. */
-		TASK_MY_TASK_PRIORITY,/* Priority at which the task is created. */
+		TASK_A_PRIORITY,/* Priority at which the task is created. */
 		NULL);      /* Used to pass out the created task's handle. */
 
 		/* Create the task, storing the handle. */
 	xTaskCreate(
-		taskMySeccondTask,       /* Function that implements the task. */
-		"MySecondTask",          /* Text name for the task. */
-		TASK_MY_SECOND_TASK_STACK,      /* Stack size in words, not bytes. */
+		taskBTask,       /* Function that implements the task. */
+		"Task B",          /* Text name for the task. */
+		TASK_B_STACK,      /* Stack size in words, not bytes. */
 		(void*)2,    /* Parameter passed into the task. */
-		TASK_MY_SECOND_TASK_PRIORITY,/* Priority at which the task is created. */
-		&_taskSecondHandle);      /* Used to pass out the created task's handle. */
+		TASK_B_PRIORITY,/* Priority at which the task is created. */
+		&_taskBHandle);      /* Used to pass out the created task's handle. */
+
+			/* Create the task, storing the handle. */
+	xTaskCreate(
+		taskCTask,       /* Function that implements the task. */
+		"Task C",          /* Text name for the task. */
+		TASK_C_STACK,      /* Stack size in words, not bytes. */
+		(void*)3,    /* Parameter passed into the task. */
+		TASK_C_PRIORITY,/* Priority at which the task is created. */
+		NULL);      /* Used to pass out the created task's handle. */
 
 
 	/* Create a binary semaphor and give it */
-	_putsSem = xSemaphoreCreateBinary(void);
-	xSemaphoreGive(_putsSem);
+	_aSem = xSemaphoreCreateBinary(void);
+	_bSem = xSemaphoreCreateBinary(void);
+	_cSem = xSemaphoreCreateBinary(void);
 
 	// Let the operating system take over :)
 	vTaskStartScheduler();
