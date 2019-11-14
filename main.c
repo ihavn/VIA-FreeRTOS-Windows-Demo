fe48b57aae710646535931d606057f2e044611a1
Ib Havn <iha@via.dk>
2019-11-14 10:45:29 +0100

    Add MessageBuffer Example

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "FreeRTOS.h"
 #include "task.h"
+#include "message_buffer.h"
 
 /* Priorities at which the tasks are created. */
 #define TASK_MY_TASK_PRIORITY			( tskIDLE_PRIORITY + 1 )
@@ -12,16 +13,27 @@
 /* Task Handles */
 TaskHandle_t _taskSecondHandle = NULL;
 
+/* Message buffer handles */
+MessageBufferHandle_t _messBuf_handle = NULL;
+#define MESS_BUF_SIZE	(500)
+
 // --------------------------------------------------------------------------------------
 void taskMyTask(void* pvParameters)
 {
 	// Remove compiler warnings.
 	(void)pvParameters;
 
+	char txBuf[40];
+	char* someText[] = { "Peter Pan", "Julia Roberts", "James Dean", "Frodo", "Gary Grant" };
+
 	for (;;)
 	{
 		vTaskDelay(pdMS_TO_TICKS(200));
-		puts("Hi from My Task");
+		for (int i = 0; i < 5; i++)
+		{
+			sprintf(txBuf, "Message no %d: %s", i, someText[i] );
+			xMessageBufferSend(_messBuf_handle, txBuf, strlen(txBuf) + 1, portMAX_DELAY);
+		}
 	}
 }
 
@@ -31,10 +43,13 @@ void taskMySeccondTask(void* pvParameters)
 	// Remove compiler warnings.
 	(void)pvParameters;
 
+	char rxBuf[40];
+	int rxLen = 0;
+
 	for (;;)
 	{
-		vTaskDelay(pdMS_TO_TICKS(100));
-		puts("Hi from My Second Task");
+		rxLen = xMessageBufferReceive(_messBuf_handle, rxBuf, 40, portMAX_DELAY);
+		printf("Received %d bytes: %s\n", rxLen, rxBuf);
 	}
 }
 
@@ -58,6 +73,8 @@ void main(void)
 		(void*)2,    /* Parameter passed into the task. */
 		TASK_MY_SECOND_TASK_PRIORITY,/* Priority at which the task is created. */
 		&_taskSecondHandle);      /* Used to pass out the created task's handle. */
+
+	_messBuf_handle = xMessageBufferCreate(MESS_BUF_SIZE);
 
 	// Let the operating system take over :)
 	vTaskStartScheduler();
