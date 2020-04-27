90f790cc09af3bd4b11ecf23e1e5d8b7177c02dc
Ib Havn <iha@via.dk>
2020-04-27 07:37:33 +0200

    Change to reuse task function

--- main.c
+++ main.c
@@ -19,65 +19,21 @@ TaskHandle_t _taskSecondHandle = NULL;
 SemaphoreHandle_t xPrintfMutex;
 
 // --------------------------------------------------------------------------------------
-void taskMyTask(void* pvParameters)
+void taskTask(void* pvParameters)
 {
-	// Remove compiler warnings.
-	(void)pvParameters;
-
+	TickType_t xFrequency[] = { 200, 300, 700 };
+	
 	TickType_t xLastWakeTime;
-	const TickType_t xFrequency = 200;
 
 	// Initialise the xLastWakeTime variable with the current time.
 	xLastWakeTime = xTaskGetTickCount();
 
 	for (;;)
 	{
-		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+		vTaskDelayUntil(&xLastWakeTime, xFrequency[(int)pvParameters]);
 		if (xSemaphoreTake(xPrintfMutex, portMAX_DELAY)) {
-			printf("Hi from My Task - ticks: %ld\n", xTaskGetTickCount());
-			xSemaphoreGive(xPrintfMutex);
-		}
-	}
-}
-
-// --------------------------------------------------------------------------------------
-void taskMySeccondTask(void* pvParameters)
-{
-	// Remove compiler warnings.
-	(void)pvParameters;
-
-	TickType_t xLastWakeTime;
-	const TickType_t xFrequency = 300;
-
-	// Initialise the xLastWakeTime variable with the current time.
-	xLastWakeTime = xTaskGetTickCount();
-
-	for (;;)
-	{
-		vTaskDelayUntil(&xLastWakeTime, xFrequency);
-		if (xSemaphoreTake(xPrintfMutex, portMAX_DELAY)) {
-			printf("Hi from My Second Task - ticks: %ld\n", xTaskGetTickCount());
-			xSemaphoreGive(xPrintfMutex);
-		}
-	}
-}
-
-// --------------------------------------------------------------------------------------
-void taskMyThirdTask(void* pvParameters)
-{
-	// Remove compiler warnings.
-	(void)pvParameters;
-
-	TickType_t xLastWakeTime;
-	const TickType_t xFrequency = 700;
-
-	// Initialise the xLastWakeTime variable with the current time.
-	xLastWakeTime = xTaskGetTickCount();
-	for (;;)
-	{
-		vTaskDelayUntil(&xLastWakeTime, xFrequency);
-		if (xSemaphoreTake(xPrintfMutex, portMAX_DELAY)) {
-			printf("Hi from My Third Task - ticks: %ld\n", xTaskGetTickCount());
+			printf("Hi from %s", pcTaskGetName(NULL));
+			printf(" - ticks: %ld\n", xTaskGetTickCount());
 			xSemaphoreGive(xPrintfMutex);
 		}
 	}
@@ -88,28 +44,28 @@ void main(void)
 {
 	/* Create the task, not storing the handle. */
 	xTaskCreate(
-		taskMyTask,       /* Function that implements the task. */
+		taskTask,       /* Function that implements the task. */
 		"MyTask",          /* Text name for the task. */
 		TASK_MY_TASK_STACK,      /* Stack size in words, not bytes. */
-		(void*)1,    /* Parameter passed into the task. */
+		(void*)0,    /* Parameter passed into the task. */
 		TASK_MY_TASK_PRIORITY,/* Priority at which the task is created. */
 		NULL);      /* Used to pass out the created task's handle. */
 
 		/* Create the task, storing the handle. */
 	xTaskCreate(
-		taskMySeccondTask,       /* Function that implements the task. */
+		taskTask,       /* Function that implements the task. */
 		"MySecondTask",          /* Text name for the task. */
 		TASK_MY_SECOND_TASK_STACK,      /* Stack size in words, not bytes. */
-		(void*)2,    /* Parameter passed into the task. */
+		(void*)1,    /* Parameter passed into the task. */
 		TASK_MY_SECOND_TASK_PRIORITY,/* Priority at which the task is created. */
 		&_taskSecondHandle);      /* Used to pass out the created task's handle. */
 
 		/* Create the task, storing the handle. */
 	xTaskCreate(
-		taskMyThirdTask,       /* Function that implements the task. */
+		taskTask,       /* Function that implements the task. */
 		"MyThirdTask",          /* Text name for the task. */
 		TASK_MY_THIRD_TASK_STACK,      /* Stack size in words, not bytes. */
-		(void*)3,    /* Parameter passed into the task. */
+		(void*)2,    /* Parameter passed into the task. */
 		TASK_MY_THIRD_TASK_PRIORITY,/* Priority at which the task is created. */
 		NULL);      /* Used to pass out the created task's handle. */
 
