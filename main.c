32453f48d407a7dd950f6a2da49a3f635280a77b
Ib Havn <iha@via.dk>
2020-04-27 07:25:12 +0200

    Initial commit

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
 /* Priorities at which the tasks are created. */
 #define TASK_MY_TASK_PRIORITY			( tskIDLE_PRIORITY + 1 )
 #define	TASK_MY_SECOND_TASK_PRIORITY	( tskIDLE_PRIORITY + 2 )
+#define	TASK_MY_THIRD_TASK_PRIORITY	( tskIDLE_PRIORITY + 3 )
 /* Task stack sizes*/
 #define TASK_MY_TASK_STACK				( configMINIMAL_STACK_SIZE )
 #define	TASK_MY_SECOND_TASK_STACK		( configMINIMAL_STACK_SIZE )
+#define	TASK_MY_THIRD_TASK_STACK		( configMINIMAL_STACK_SIZE )
 
 /* Task Handles */
 TaskHandle_t _taskSecondHandle = NULL;
+
+/* Semaphore Handles */
+SemaphoreHandle_t xPrintfMutex;
 
 // --------------------------------------------------------------------------------------
 void taskMyTask(void* pvParameters)
@@ -18,10 +24,19 @@ void taskMyTask(void* pvParameters)
 	// Remove compiler warnings.
 	(void)pvParameters;
 
+	TickType_t xLastWakeTime;
+	const TickType_t xFrequency = 200;
+
+	// Initialise the xLastWakeTime variable with the current time.
+	xLastWakeTime = xTaskGetTickCount();
+
 	for (;;)
 	{
-		vTaskDelay(pdMS_TO_TICKS(200));
-		puts("Hi from My Task");
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+		if (xSemaphoreTake(xPrintfMutex, portMAX_DELAY)) {
+			printf("Hi from My Task - ticks: %ld\n", xTaskGetTickCount());
+			xSemaphoreGive(xPrintfMutex);
+		}
 	}
 }
 
@@ -31,10 +46,40 @@ void taskMySeccondTask(void* pvParameters)
 	// Remove compiler warnings.
 	(void)pvParameters;
 
+	TickType_t xLastWakeTime;
+	const TickType_t xFrequency = 300;
+
+	// Initialise the xLastWakeTime variable with the current time.
+	xLastWakeTime = xTaskGetTickCount();
+
 	for (;;)
 	{
-		vTaskDelay(pdMS_TO_TICKS(100));
-		puts("Hi from My Second Task");
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+		if (xSemaphoreTake(xPrintfMutex, portMAX_DELAY)) {
+			printf("Hi from My Second Task - ticks: %ld\n", xTaskGetTickCount());
+			xSemaphoreGive(xPrintfMutex);
+		}
+	}
+}
+
+// --------------------------------------------------------------------------------------
+void taskMyThirdTask(void* pvParameters)
+{
+	// Remove compiler warnings.
+	(void)pvParameters;
+
+	TickType_t xLastWakeTime;
+	const TickType_t xFrequency = 700;
+
+	// Initialise the xLastWakeTime variable with the current time.
+	xLastWakeTime = xTaskGetTickCount();
+	for (;;)
+	{
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+		if (xSemaphoreTake(xPrintfMutex, portMAX_DELAY)) {
+			printf("Hi from My Third Task - ticks: %ld\n", xTaskGetTickCount());
+			xSemaphoreGive(xPrintfMutex);
+		}
 	}
 }
 
@@ -58,6 +103,18 @@ void main(void)
 		(void*)2,    /* Parameter passed into the task. */
 		TASK_MY_SECOND_TASK_PRIORITY,/* Priority at which the task is created. */
 		&_taskSecondHandle);      /* Used to pass out the created task's handle. */
+
+		/* Create the task, storing the handle. */
+	xTaskCreate(
+		taskMyThirdTask,       /* Function that implements the task. */
+		"MyThirdTask",          /* Text name for the task. */
+		TASK_MY_THIRD_TASK_STACK,      /* Stack size in words, not bytes. */
+		(void*)3,    /* Parameter passed into the task. */
+		TASK_MY_THIRD_TASK_PRIORITY,/* Priority at which the task is created. */
+		NULL);      /* Used to pass out the created task's handle. */
+
+	/* Create Mutex */
+	xPrintfMutex = xSemaphoreCreateMutex();
 
 	// Let the operating system take over :)
 	vTaskStartScheduler();
